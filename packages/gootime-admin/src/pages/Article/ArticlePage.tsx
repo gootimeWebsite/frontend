46c2057dd13510a7834012c194015cfdf4264080
sholsyjy <yangjy15@mails.tsinghua.edu.cn>
2019-02-05 17:09:01 +0800

    ✨ feat: article form

--- packages/gootime-admin/src/pages/Article/ArticlePage.tsx
+++ packages/gootime-admin/src/pages/Article/ArticlePage.tsx
@@ -1,22 +1,40 @@
 import * as React from 'react';
 import { useState } from 'react';
-import * as cookie from 'react-cookies';
-import useReactRouter from 'use-react-router';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Input, Button, Select } from 'antd';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import AdminHeader from '../../components/AdminHeader';
 import AdminList from '../../components/AdminList';
 import styles from './styles.module.scss';
 
-const { Sider, Header, Footer, Content } = Layout;
+const { Sider, Content } = Layout;
+const { Option } = Select;
+
+const groups = [
+  ['entertainment', '娱乐'],
+  ['video', '影视'],
+  ['fasion', '时尚'],
+  ['makeup', '美妆'],
+  ['activity', '活动'],
+  ['bibi', '哔哔']
+];
+
+export type ArticleType = {
+  title: string;
+  group: 'entertainment' | 'video' | 'fasion' | 'makeup' | 'activity' | 'bibi';
+  author: string;
+  content: string;
+};
+
+const defaultForm: ArticleType = {
+  title: '',
+  group: 'entertainment',
+  author: '',
+  content: ''
+};
 
 const ArticlePage: React.FC = () => {
-  const { history } = useReactRouter();
-  const logout = () => {
-    cookie.remove('token');
-    history.replace('/login');
-  };
+  const [form, setForm] = useState<ArticleType>(defaultForm);
 
   const modules = {
     toolbar: [
@@ -47,6 +65,27 @@ const ArticlePage: React.FC = () => {
     'image'
   ];
 
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const newForm = { ...form, title: e.target.value };
+    setForm(newForm);
+  };
+
+  const handleAuthor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const newForm = { ...form, author: e.target.value };
+    setForm(newForm);
+  };
+
+  const handleGroup = (value: string[]) => {
+    const newForm = { ...form, group: value[0] };
+    setForm(newForm as ArticleType);
+  };
+
+  const saveArticle = () => {
+    console.log('save form', form);
+  };
+
   return (
     <div>
       <AdminHeader username="admin" />
@@ -54,7 +93,45 @@ const ArticlePage: React.FC = () => {
         <Sider theme={'light'} className={styles.sider}>
           <AdminList />
         </Sider>
-        <Content>
+        <Content className={styles.content}>
+          <Row>
+            <Col span={1}>
+              <span>标题:</span>
+            </Col>
+            <Col span={10}>
+              <Input onChange={handleTitle} placeholder={'请输入标题'} />
+            </Col>
+            <Col span={9}>
+              <Button type={'primary'} onClick={saveArticle}>
+                保存
+              </Button>
+            </Col>
+            <Col span={2}>
+              <Button type={'primary'} onClick={saveArticle}>
+                保存草稿
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={1}>
+              <span>分组:</span>
+            </Col>
+            <Col span={10}>
+              <Select defaultValue={groups[0]} onSelect={handleGroup}>
+                {groups.map(group => (
+                  <Option key={group[0]} value={group[0]}>
+                    {group[1]}
+                  </Option>
+                ))}
+              </Select>
+            </Col>
+            <Col span={1}>
+              <span>作者:</span>
+            </Col>
+            <Col span={10}>
+              <Input onChange={handleAuthor} placeholder={'请输入作者'} />
+            </Col>
+          </Row>
           <ReactQuill
             className={styles.quill}
             theme={'snow'}
